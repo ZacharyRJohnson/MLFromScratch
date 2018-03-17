92aa4b9a68e49ed4f7c9a3f6418ffad2823773c4
Zachary Johnson <zachjohnson1900@gmail.com>
2018-03-16 21:02:43 -0400

    Finished up tutorial code

--- LinearRegression/LeastSquareMethod.py
+++ LinearRegression/LeastSquareMethod.py
@@ -13,6 +13,7 @@ data.head()
 X = data['Head Size(cm^3)'].values
 Y = data['Brain Weight(grams)'].values
 
+#Calculating coefficient and bias coefficient for linear model
 mean_x = np.mean(X)
 mean_y = np.mean(Y)
 
@@ -27,3 +28,38 @@ b1 = numer/denom
 b0 = mean_y - (b1 * mean_x)
 
 print(b0, b1)
+#Model: BrainWeight = b0 + b1*HeadSize
+
+#Showing graphically
+
+max_x = np.max(X) + 100
+min_x = np.min(X) - 100
+
+x = np.linspace(min_x, max_x, 1000)
+y = b0 + b1*x
+
+plt.plot(x, y, color='#58b970', label='Regression Line')
+plt.scatter(X, Y, c='#ef5423', label='Scatter Plot')
+
+plt.xlabel('Head Size in cm3')
+plt.ylabel('Brain Weight in grams')
+plt.legend()
+plt.show()
+
+#Calculating Root Mean Squares Error
+rmse = 0
+for i in range(m):
+    y_pred = b0 + b1 * X[i]
+    rmse += (Y[i] - y_pred) ** 2
+rmse = np.sqrt(rmse/m)
+print(rmse)
+
+#Calculating R squared score
+ss_t = 0
+ss_r = 0
+for i in range(m):
+    y_pred = b0 + b1 * X[i]
+    ss_t += (Y[i] - mean_y) ** 2
+    ss_r += (Y[i] - y_pred) ** 2
+r2 = 1 - (ss_r/ss_t)
+print(r2)
